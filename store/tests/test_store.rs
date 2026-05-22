fcd2f0d0dcc52ef8b27c02adbfe60d04cf4d3ff7
Gabriel <gabrielpfernandez@gmail.com>
2026-05-22 15:39:02 +0200

    test(store): Complete basic round trip store test.

--- store/tests/test_store.rs
+++ store/tests/test_store.rs
@@ -12,8 +12,6 @@ mod tests {
         let mut store = Store::new(None);
         assert!(store.store(str_chain.as_str()).is_ok());
         let str_chain = store.load().unwrap();
-        //assert!(res.is_ok());
-        //let str_chain = res.unwrap();
         let new_chain: Chain = serde_json::from_str(&str_chain).unwrap();
         assert_eq!(chain, new_chain);
     }
